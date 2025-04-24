2532243b668485ed208a4c8ba3882ea444d54c3b
Justin Nguyen <iamaalt921@gmail.com>
2025-04-23 19:49:53 -0700

    Removed Previous Changes

--- CalcAB/DerivativeDef/derivativeDef.py
+++ CalcAB/DerivativeDef/derivativeDef.py
@@ -1,6 +1,6 @@
 from manim import *
-import numpy as np
-
+import math
+#manim -pqh derivativeDef.py DerivativeDef
 class DerivativeDef(Scene):
     def construct(self):
         axes = Axes(
@@ -15,47 +15,27 @@ class DerivativeDef(Scene):
             stroke_color=BLUE     
         )
 
-        self.play(Write(axes), run_time=2)
-        self.play(Create(sin_graph), run_time=2)
+        self.play(Write(axes), run_time = 2)
+        self.play(Create(sin_graph, run_time=2))
         self.wait()
 
-        # Initial x positions for dots
-        x_a = 0.3
-        x_b = 2.0
-        
-        dota = Dot(color=RED).move_to(axes.i2gp(x_a, sin_graph))
-        dotb = Dot(color=RED).move_to(axes.i2gp(x_b, sin_graph))
-        self.play(FadeIn(dota, scale=0.5), FadeIn(dotb, scale=0.5))
+        dota = Dot(color = RED)
+        dota.move_to(axes.i2gp(0.3, sin_graph))
+        self.play(FadeIn(dota,scale=0.5))
 
-        # Create secant line group using x-coordinates, not positions
-        secant_group = axes.get_secant_slope_group(
-            graph=sin_graph,
-            x=x_a,
-            dx=x_b - x_a,  # Difference in x-coordinates
-            secant_line_color=PURPLE,
-            secant_line_length=6
+        dotb = Dot(color = RED)
+        dotb.move_to(axes.i2gp(2, sin_graph))
+        self.play(FadeIn(dotb,scale=0.5))
+
+        dotb_tracker = ValueTracker(2)
+        f_always(
+            dotb.move_to,
+            lambda: axes.i2gp(dotb_tracker.get_value(), sin_graph)
         )
-        self.play(FadeIn(secant_group), run_time=2)
-        self.wait()
 
-        # Animate dotb moving
-        dotb_tracker = ValueTracker(x_b)
-        dotb.add_updater(lambda m: m.move_to(
-            axes.i2gp(dotb_tracker.get_value(), sin_graph)
-        ))
-        
-        # Update secant line during animation
-        def update_secant(group):
-            new_group = axes.get_secant_slope_group(
-                graph=sin_graph,
-                x=x_a,
-                dx=dotb_tracker.get_value() - x_a,
-                secant_line_color=PURPLE,
-                secant_line_length=6
-            )
-            group.become(new_group)
-        
-        secant_group.add_updater(update_secant)
-        
-        self.play(dotb_tracker.animate.set_value(0.3), run_time=5)
+       
+        derv = axes.get_secant_slope_group(graph = sin_graph, x = dota.get_center(), dx = dotb.get_center() - dota.get_center(), secant_line_color = PURPLE, secant_line_length = 6)
+        self.play(FadeIn(derv), run_time = 2)
+        self.wait()
+        self.play(dotb_tracker.animate.set_value(0.3), run_time = 5)
         self.wait()
