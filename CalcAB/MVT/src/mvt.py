3224d306cb6976002786541d3d58c6478c41575d
YourJustin Nguyen <you@exampliamaalt921@gmail.com>
2025-04-29 10:55:00 -0700

    Added labels for both the secant line and the average slope line

--- CalcAB/MVT/src/mvt.py
+++ CalcAB/MVT/src/mvt.py
@@ -19,16 +19,21 @@ class mvt(Scene):
 
         avgSlope = axes.plot(
             lambda x: x,
-            stroke_color = GREEN
+            stroke_color = GREEN,
+            x_range=[-1, 4]
         )
-
-        mvtAb = Text(r"The MVT")
-        mvtExp = Text(r"The Mean Value Theorum")
-        mvtDef = Text("for a continuous and differentiable function, there exists \n a point where the derivative equals the average \n rate of change over an interval").scale(0.8)
-        mvtAbP2 = Text(r"The MVT").shift(UP*1.5)
 
         x = ValueTracker(0) #Go up till x = 2
         derv = always_redraw(lambda: axes.get_secant_slope_group(graph=func, x = x.get_value(), dx = 0.01, secant_line_length=10, secant_line_color=YELLOW))
+        dotA = Dot(color=PURE_RED).move_to(axes.i2gp(0, avgSlope))
+        dotB = Dot(color=PURE_RED).move_to(axes.i2gp(3, avgSlope))
+
+        mvtAb = Text(r"The MVT")
+        mvtExp = Text(r"The Mean Value Theorum")
+        mvtDef = Text("For a continuous and differentiable function, there exists \n a point where the derivative equals the average \n rate of change over an interval").scale(0.8)
+        mvtAbP2 = Text(r"The MVT").shift(UP*1.5)
+        avgSlopeLabel  = MathTex(r"slope = \frac{y_{2}-y_{1}}{x_{2}-x_{1}} = 1").next_to(axes.i2gp(2, avgSlope), DOWN*1.3)
+        dervLabel = MathTex(r"f'(2) = 1").next_to(axes.i2gp(2, func), DOWN)
         
         self.play(Write(mvtAb))
         self.wait(1.5)
@@ -39,8 +44,12 @@ class mvt(Scene):
         self.play(ReplacementTransform(mvtExp, mvtAbP2), FadeOut(mvtDef))
         self.play(mvtAbP2.animate.to_edge(UP, buff=1), Write(axes), Write(axes_label), Write(func))
         self.wait(1.5)
-        self.play(Write(avgSlope))
+        self.play(Write(avgSlope), Create(dotA), Create(dotB))
+        self.wait(1.5)
+        self.play(Write(avgSlopeLabel))
         self.wait(1.5)
         self.play(Write(derv))
-        self.play(x.animate.set_value(2))
+        self.play(x.animate.set_value(2), run_time = 6)
+        self.wait(1.5)
+        self.play(Write(dervLabel))
         self.wait(1.5)
