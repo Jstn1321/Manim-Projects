5cce551543bcffb92b159548c7679d21fa6b9bf0
Justin Nguyen <iamaalt921@gmail.com>
2025-08-07 22:14:45 -0700

    Added p1 second move and p2 third move

--- Game Theory/TitForTat/src/titfortat.py
+++ Game Theory/TitForTat/src/titfortat.py
@@ -18,7 +18,10 @@ class titfortat(Scene):
         firstStep = Tex("The first move is to cooperate").to_edge(UP, buff=1)
         secondStep = Tex("The next step is to copy each step the opponent makes").to_edge(UP, buff=1)
         p2FirstMoveText = Tex("If p2 refuses to cooperate...").to_edge(UP, buff=1)
-        p1SecondMoveText = Tex("then p1 will copy his move!").to_edge(UP, buff=1)
+        p1SecondMoveText = Tex("then p1 will also defect!").to_edge(UP, buff=1)
+        p2SecondMoveText = Tex("If p2 wants to cooperate...").to_edge(UP, buff=1)
+        p1ThirdMoveText = Tex("then p1 will also cooperate!").to_edge(UP, buff=1)
+        p1RestofGameText = Tex("After the first move p1 will copy what ever move p2 makes")
         # Create the empty table first
         empty_values = [["" for _ in range(7)] for _ in range(2)]  # Create a blank table with the same dimensions
         empty_table = Table(
@@ -29,9 +32,12 @@ class titfortat(Scene):
             include_outer_lines=True,
             arrange_in_grid_config={"cell_alignment": RIGHT}
         )
-        firstfirstcell = empty_table.get_cell((2, 2))  
-        p2FirstMove = empty_table.get_cell((3, 2))
+        p1FirstMove = empty_table.get_cell((2, 2))  
         p1SecondMove = empty_table.get_cell((2,3))
+        p1ThirdMove = empty_table.get_cell((2, 4))
+        p2FirstMove = empty_table.get_cell((3, 2))
+        p2SecondMove = empty_table.get_cell((3, 3))
+        
         self.wait(1)
         self.play(Write(title))
         self.wait(1.5)
@@ -40,11 +46,15 @@ class titfortat(Scene):
         self.wait(6)
         self.play(ReplacementTransform(title, consider), ReplacementTransform(titfortatDef, empty_table))
         self.wait(1.5)
-        self.play(ReplacementTransform(titfortatDef, Text("C").move_to(firstfirstcell.get_center())), ReplacementTransform(consider, firstStep))
+        self.play(ReplacementTransform(titfortatDef, Text("C").move_to(p1FirstMove.get_center())), ReplacementTransform(consider, firstStep))
         self.wait(2)
         self.play(ReplacementTransform(firstStep, secondStep))
         self.wait(3)
         self.play(ReplacementTransform(secondStep, p2FirstMoveText), Write(Text("D").move_to(p2FirstMove.get_center())))
         self.wait(4)
         self.play(ReplacementTransform(p2FirstMoveText, p1SecondMoveText), Write(Text("D").move_to(p1SecondMove.get_center())))
+        self.wait(2)
+        self.play(ReplacementTransform(p1SecondMoveText, p2SecondMoveText), Write(Text("C").move_to(p2SecondMove.get_center())))
+        self.wait(4)
+        self.play(ReplacementTransform(p2SecondMoveText, p1ThirdMoveText), Write(Text("C").move_to(p1ThirdMove.get_center())))
         self.wait(2)
