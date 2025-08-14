8ce30bdb5b4d658cd76a8da5b41e336bef70810a
YourJustin Nguyen <you@exampliamaalt921@gmail.com>
2025-08-14 14:58:58 -0700

    Added title and appropriate files needed for compilation

---
+++ PaulsNotes/Calculus 2/Unit 10 - Sequences/src/sequences.py
@@ -0,0 +1,10 @@
+from manim import *
+
+class sequences(Scene):
+    def construct(self):
+        classTitle = Tex("Calculus 2/BC").to_edge(UP, buff=1)
+        title = Tex("Sequences").scale(2)
+
+        self.play(Write(classTitle))
+        self.play(Write(title))
+        self.wait(2)
