ae2c4faaed68ebbaa6288e84a736259837a0a6b5
YourJustin Nguyen <you@exampliamaalt921@gmail.com>
2025-08-20 18:42:00 -0700

    Changed the title to Series

--- PaulsNotes/Calculus 2/Unit 10 - Sequences/src/sequences.py
+++ PaulsNotes/Calculus 2/Unit 10 - Sequences/src/sequences.py
@@ -3,7 +3,7 @@ from manim import *
 class sequences(Scene):
     def construct(self):
         classTitle = Tex("Calculus 2/BC").to_edge(UP, buff=1)
-        title = Tex("Sequences").scale(2)
+        title = Tex("Series").scale(2)
 
         self.play(Write(classTitle))
         self.play(Write(title))
